bad6b95db5f3d7c23540dd239d936b27883771ca
LDLcodes <153458380+LDLcodes@users.noreply.github.com>
2025-05-07 02:23:09 +0900

    erase unnecessary imports and fixing deps

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,7 @@ function App() {
     return () => {
       document.removeEventListener("keypress", handleKeyDown);
     };
-  }, [guessedLetters]); // Check if the key pressed is a letter
+  }, [guessedLetters, addGuessedLetter]); // Check if the key pressed is a letter
 
   return (
     <>
