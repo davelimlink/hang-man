7c4a1fea13f22b88f8ca8e613850553364ec3935
LDLcodes <153458380+LDLcodes@users.noreply.github.com>
2025-05-07 02:31:18 +0900

    erase deps

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,7 @@ function App() {
     return () => {
       document.removeEventListener("keypress", handleKeyDown);
     };
-  }, [guessedLetters, addGuessedLetter]); // Check if the key pressed is a letter
+  }, [guessedLetters]); // Check if the key pressed is a letter
 
   return (
     <>
